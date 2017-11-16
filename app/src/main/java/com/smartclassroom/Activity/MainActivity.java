a1b528e4572bd6bf53219b79a4b8e15750511fc2
Chanpreet Singh Chhabra <chanpreet.chhabra@gmail.com>
2017-11-16 22:24:33 +0530

    new folder windows

--- app/src/main/java/com/smartclassroom/Activity/MainActivity.java
+++ app/src/main/java/com/smartclassroom/Activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.smartclassroom;
+package com.smartclassroom.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.smartclassroom.R;
 
 public class MainActivity extends AppCompatActivity {
 
